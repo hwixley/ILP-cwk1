5189139cdabf9fe4583f5b311d05d227d8fd10e5
hwixley <hwixley1@gmail.com>
2020-09-23 14:20:51 +0100

    upd

--- heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -8,7 +8,7 @@ public class App
     public static void main( String[] args ) throws Exception
     {	
     	//Retrieve predictions.txt as command line argument
-    	String predFilePath = args[0];
+    	String predFilePath = "/home/hwixley/Documents/Year3/ILP/cwk1/heatmap/src/main/java/uk/ac/ed/inf/heatmap/predictions.txt"; //args[0];
         
     	//Read the predictions.txt file using BufferedReader
         File predFile = new File(predFilePath);
@@ -21,6 +21,31 @@ public class App
         //Iterate through the lines of the predictions.txt file and store them as Integers in the 'predictions' ArrayList
         String line;
         while ((line = br.readLine()) != null) {
+        	fileLine.clear();
+        	
+        	Boolean lineEnd = false;
+        	Integer commaIndex;
+        	Integer startIndex = 0;
+        	String restOfLine = line;
+        	String subString;
+        	
+        	while (!lineEnd) {
+        		commaIndex = restOfLine.indexOf(",", startIndex);
+        		
+        		if (commaIndex == -1) {
+        			subString = restOfLine;
+            		fileLine.add(Integer.parseInt(subString));
+        			lineEnd = true;
+        		} else {
+            		subString = restOfLine.substring(startIndex, commaIndex);
+            		fileLine.add(Integer.parseInt(subString));
+            		startIndex = commaIndex + 1;
+        		}
+        		fileLine.add(Integer.parseInt(subString));
+        		
+        		
+        		//restOfLine = 
+        	}
         	System.out.println(line);
         }
     }
