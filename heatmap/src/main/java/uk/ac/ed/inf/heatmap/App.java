eca27678bedbb3f960da44ec993306cc5a9826af
hwixley <hwixley1@gmail.com>
2020-09-23 14:51:52 +0100

    upd

--- heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -15,7 +15,7 @@ public class App
         BufferedReader br = new BufferedReader(new FileReader(predFile));
         
         //Create ArrayLists to store the predictions.txt data
-        ArrayList<Integer> predictions = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> predictions = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> fileLine = new ArrayList<Integer>();
         
         //Iterate through the lines of the predictions.txt file and store them as Integers in the 'predictions' ArrayList
@@ -29,24 +29,28 @@ public class App
         	String restOfLine = line;
         	String subString;
         	
+        	//Iterates through the predictions on each line and stores them in 'fileLine' which is then added to 'predictions'
         	while (!lineEnd) {
+        		startIndex = 0;
         		commaIndex = restOfLine.indexOf(",", startIndex);
         		
+        		//Checks if there are no more commas found (meaning the last entry on the given parsed file line)
         		if (commaIndex == -1) {
         			subString = restOfLine;
             		fileLine.add(Integer.parseInt(subString));
         			lineEnd = true;
+        			break;
         		} else {
             		subString = restOfLine.substring(startIndex, commaIndex);
             		fileLine.add(Integer.parseInt(subString));
-            		startIndex = commaIndex + 1;
+            		startIndex = commaIndex + 2;
+            		restOfLine = restOfLine.substring(startIndex, restOfLine.length());
         		}
-        		fileLine.add(Integer.parseInt(subString));
-        		
-        		
-        		//restOfLine = 
         	}
-        	System.out.println(line);
+        	//All predictions for the given line ('fileLine') have been parsed as Integers and added to 'predictions'
+        	predictions.add((ArrayList<Integer>) fileLine.clone());
         }
+        
+        
     }
 }
