deaef205d19025c5329fa30f8e9074f83205be70
hwixley <hwixley1@gmail.com>
2020-09-23 13:55:56 +0100

    reformatting predictions.txt

--- heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -1,19 +1,18 @@
 package uk.ac.ed.inf.heatmap;
 
 import java.io.*;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args ) throws Exception
-    {
+    {	
+    	//Retrieve predictions.txt as command line argument
     	String predFilePath = args[0];
         
         File predFile = new File(predFilePath);
         BufferedReader br = new BufferedReader(new FileReader(predFile));
-
+        
+        //Read predictions.txt file and output data into 'predictions' String
         String predictions = br.readLine();
         System.out.println(predictions);
     }
